8364bb73b6339e2c4b2bae82554f119ee3805472
Ajay Chaudhari <55138445+Ajay-Chaudhari01001@users.noreply.github.com>
2023-06-23 22:39:36 +0530

    Update NotFound.jsx

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,6 @@
 const NotFound = () => {
   return (
     <div className="flex justify-center items-center h-[80vh] gap-3">
-      <div>
-      <img src="/contact.png" alt="contact_png" />
-      </div>
       <h3 className="text-center text-1xl text-white">Contact not found</h3>
     </div>
   )
